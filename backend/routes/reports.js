e8df6bee508f810d8080add6d6b9187cdae3ac95
Oleh Kir <oleggio.kir@gmail.com>
2019-05-11 20:23:22 +0300

    Added methods, fixed periodMode missing value

--- backend/routes/reports.js
+++ backend/routes/reports.js
@@ -11,6 +11,20 @@ router.get('/:id/execs', defaultErrorHandling(function (req, res) {
         })
 }));
 
+router.put('/', defaultErrorHandling(function (req, res) {
+    return reportservice.updateReport(req.body)
+        .then(rep => {
+            res.json(rep);
+        })
+}));
+
+router.delete('/:id', defaultErrorHandling(function (req, res) {
+    return reportservice.deleteReport(req.params.id)
+        .then(() => {
+            res.json({status: "Ok"});
+        })
+}));
+
 router.post('/', defaultErrorHandling(function (req, res) {
     const reportData = req.body;
 
