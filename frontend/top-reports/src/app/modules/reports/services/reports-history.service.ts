2d862087265a5c77e1a7b30e3f532c6867d4b880
yuriiivaskevych <ivaskevychuriy@gmail.com>
2019-05-15 22:04:55 +0300

    remove mocks

--- frontend/top-reports/src/app/modules/reports/services/reports-history.service.ts
+++ frontend/top-reports/src/app/modules/reports/services/reports-history.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 import { ReportResult } from '../models/report-result';
-import { of, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,32 +13,9 @@ export class ReportsHistoryService {
 
   public get(reportId: number) : Observable<ReportResult[]> {
     return this.http.get<ReportResult[]>('reports/' + reportId + '/execs');
-
-    // MOCKED
-    const data: ReportResult[] = [
-      { 
-        key: '1',
-        date: '2019-05-15',
-        time: '21-23-00',
-        fileName: 'abc1'
-      },
-      { 
-        key: '2',
-        date: '2019-05-14',
-        time: '21-33-00',
-        fileName: 'test 123 a'
-      }
-    ];
-
-    return of(data);
   }
   
-  public getResult(fileName: string) : Observable<any> {
+  public getResult(fileName: string) : Observable<Blob> {
     return this.http.get('reports/download/' + fileName, { responseType: 'blob' });
-
-    // MOCKED
-    const data = new Blob(['hello there'], { type: 'text/csv' });
-
-    return of(data);
   }
 }
--- frontend/top-reports/src/app/modules/reports/services/reports.service.ts
+++ frontend/top-reports/src/app/modules/reports/services/reports.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Report } from '../models/report';
-import { of, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,35 +13,6 @@ export class ReportsService {
 
   public get() : Observable<Report[]> {
     return this.http.get<Report[]>('reports/');
-
-    // MOCKED
-    const data: Report[] = [
-      { 
-        id: 1,
-        name: 'Users count', 
-        startDate: new Date(2019, 5, 15, 0, 0, 0).toISOString(),
-        periodMode: 'd',
-        execTime: '13-30-00',
-        query: 
-`SELECT 
-  COUNT(1) AS [Count] 
-FROM [dbo].[Users]`
-      },
-      { 
-        id: 2, 
-        name: 'Names and emails', 
-        startDate: new Date(2019, 5, 16, 0, 0, 0).toISOString(),
-        periodMode: 'o',
-        execTime: '04-20-00',
-        query: 
-`SELECT 
-  [Name],
-  [Email]
-FROM [dbo.Users]`, 
-      }
-    ];
-
-    return of(data);
   }
   
   public edit(report: Report) : Observable<Report> {
@@ -51,10 +22,6 @@ FROM [dbo.Users]`,
     }
     
     return this.http.put<Report>('reports/', report);
-
-    // MOCKED
-    const data: Report = { ...report };
-    return of(data);
   }
   
   public add(report: Report) : Observable<Report> {
@@ -64,18 +31,10 @@ FROM [dbo.Users]`,
     }
 
     return this.http.post<Report>('reports/', report);
-    
-    // MOCKED
-    const data: Report = { ...report };
-    data.id = Math.round(Math.random() * 1000000);
-    return of(data);
   }
   
   // MOCKED
   public delete(id: number) : Observable<any> {
     return this.http.delete('reports/' + id);
-
-    // MOCKED
-    return of({});
   }
 }
